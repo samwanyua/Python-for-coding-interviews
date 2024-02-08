85ac6bae680cdec7e9a5fd7ac1fc24954589b680
samwanyua <samexample8@gmail.com>
2024-02-09 01:02:32 +0300

    Loops

---
+++ loops.py
@@ -0,0 +1,14 @@
+# While loops
+n = 0
+while n < 23:
+    print(n)
+    n += 3
+
+# For loop
+# Looping fron i = 23 to i = 78
+for i in range(23,78):
+    print (i)
+# Reverse
+for k in range(78,23,-2):
+    print (k)
+
