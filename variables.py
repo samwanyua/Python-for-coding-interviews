6993967d86d960f299242d9917bd9f2adaa6d755
samwanyua <samexample8@gmail.com>
2024-02-09 01:03:37 +0300

    Variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Multiple assignments
+n,m, z = 0, "abc", False
+
+print(n,m,z)
+
+# Incrementing
+n = n + 1 #good
+n +=1 #good
+# n++ #bad
+
+# None is null in python -> absence of value
+x = 4
+x = None
+print('x=', x)
