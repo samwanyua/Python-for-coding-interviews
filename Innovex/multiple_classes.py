eedaba7eada58cb1867abb1935cae45163c66bea
samwanyua <samexample8@gmail.com>
2024-07-25 16:40:00 +0300

    OOP in Python

---
+++ Innovex/multiple_classes.py
@@ -0,0 +1,30 @@
+# OOP in Python
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade  # 0 to 100
+
+    def get_grade(self):
+        return self.grade
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+        # self.is_active = False
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+
+
+
