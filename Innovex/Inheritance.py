e3c8b4536a1cd0273499ff62a69afd082c7f71ac
samwanyua <samexample8@gmail.com>
2024-07-25 17:56:09 +0300

    Inheritance   in Python

---
+++ Innovex/Inheritance.py
@@ -0,0 +1,25 @@
+# Inheritance
+class Pet:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def show(self):
+        print(f"I am {self.name}, and I am {self.age} years old")
+
+
+class Cat(Pet):
+    def sound(self):
+        print("Meow!")
+
+
+class Dog(Pet):
+    def sound(self):
+        print("Barks!")
+
+
+pet = Pet("Tim", 32)
+pet.show()
+
+cat = Cat("Mews", 4)
+cat.show()
