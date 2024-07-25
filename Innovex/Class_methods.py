5b44b2a09141f51b7dfdd4a77729dbe9a2e9b795
samwanyua <samexample8@gmail.com>
2024-07-25 19:05:17 +0300

    Introduction to class methods  in Python

--- Innovex/Class_methods.py
+++ Innovex/Class_methods.py
@@ -17,3 +17,18 @@ Class methods:
     - can access/modify class attributes
     - called on the class or an instance
 '''
+
+
+class Person:
+    number_of_people = 0  # class attribute
+
+    def __init__(self, name):
+        self.name = name
+
+
+person1 = Person("Sam")
+person2 = Person("Jim")
+
+Person.number_of_people = 89  # accessing and modifying a class attribute
+print(Person.number_of_people)
+
