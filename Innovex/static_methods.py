e0c945afffd95d07ffaf55f143b8256bc7fd9424
samwanyua <samexample8@gmail.com>
2024-07-25 19:50:04 +0300

    Introduction to static  methods  in Python

---
+++ Innovex/static_methods.py
@@ -0,0 +1,18 @@
+# static methods - belongs to a class rather than instance of the class
+'''
+- they don't require access to instance or class itself
+- can be called on both the class itself and the instance of the class
+'''
+
+
+class Math:
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    @staticmethod
+    def multiply(x, y):
+        return x * y
+
+
+print(Math.add(34, 23))
