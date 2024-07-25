f899772b502af03691194a3719dc8f32e52eb82d
samwanyua <samexample8@gmail.com>
2024-07-25 19:01:05 +0300

    Introduction to class methods  in Python

---
+++ Innovex/Class_methods.py
@@ -0,0 +1,19 @@
+# Class methods and attributes
+'''
+Class attributes - attributes shared by all instances of a class
+Defined inside a class but outside any instance methods
+Instance attributes - are unique to each object created from the class
+Class attributes are the same for all instances
+
+Instance methods:
+    - bound to the instance of the class,
+    - 'self' calls the method
+    - can access/modify instance attributes
+    - called on an instance
+
+Class methods:
+    - bound to the class itself
+    - 'cls' class itself
+    - can access/modify class attributes
+    - called on the class or an instance
+'''
