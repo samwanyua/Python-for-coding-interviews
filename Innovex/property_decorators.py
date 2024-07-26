b98453fd5f90842a1010f11456b1e41cf68de813
samwanyua <samexample8@gmail.com>
2024-07-26 13:45:37 +0300

    Implement setters  in Python

--- Innovex/property_decorators.py
+++ Innovex/property_decorators.py
@@ -9,13 +9,24 @@ class Employee:
     def email(self):
         return f"{self.first}.{self.last}@gmail.com".lower()
 
+    @property
     def fullname(self):
         return f"{self.first} {self.last}"
     def __str__(self):
         return f"Meet {self.first} {self.last}, he earns {self.pay}. Contact:  {self.email}"
+
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
 
 emp_1 = Employee("John", "Smith", 345000)
 print(emp_1.__str__())
 emp_1.first = "Jim"
 print(emp_1.__str__())
+print(emp_1.fullname)
+
+emp_1.fullname = "Barrack Obama"
+print(emp_1.fullname)
