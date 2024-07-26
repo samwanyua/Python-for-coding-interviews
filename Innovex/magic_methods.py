63d03442a4ff655abffc07984c44a455cdb01cf4
samwanyua <samexample8@gmail.com>
2024-07-26 09:13:01 +0300

    Implement __repr__ and __str__  methods  in Python

--- Innovex/magic_methods.py
+++ Innovex/magic_methods.py
@@ -1,5 +1,17 @@
+# Special methods/magic/dunder methods
+"""
+- are predefined methods that you can define in your classes
+ to provide specific behavior to instances of those classes.
+
+- surrounded by underscores ex. `__init__`, `__str__`
+- they allow you to emulate built-in types, implement operator overloading,
+and provide additional functionality
+"""
+
+
 class Employee:
     raise_amount = 1.04  # class attribute
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -7,14 +19,27 @@ class Employee:
 
     def fullname(self):
         return f"{self.first} {self.last}"
+
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    # `__repr__` is used to unambiguously represent objects - used for debugging and logging
+    def __repr__(self):
+        return f"Employee({self.first} {self.last} {self.pay})"
+
+    # `__str__` is used for readable representation of objects, meant for enduser
+    def __str__(self):
+        return f"Employee({self.first} {self.last} {self.pay})"
+
 
 employee_1 = Employee("Sam", "Tim", 30000)
-employee_2 = Employee("Rey", "Yatch", 37000)
+employee_2 = Employee("Rey", "John", 37000)
 
 print(employee_1.fullname())
 print(employee_2.pay)
 employee_2.apply_raise()
 print(employee_2.pay)
+# print(employee_2)
+
+print(repr(employee_2))
+print(str(employee_1))
