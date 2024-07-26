b0abf50a943c3dc2246af8cf5a9931c66755f534
samwanyua <samexample8@gmail.com>
2024-07-26 14:19:36 +0300

    Implement return  a book method  in Python

--- Innovex/library_system.py
+++ Innovex/library_system.py
@@ -66,8 +66,13 @@ class Member:
 
     def borrow_book(self, book):
         if book.borrow():
-            self._borrowed_books.append(book)
+            self.borrowed_books.append(book)
             return True
         return False
 
+    def return_book(self, book):
+        if book in self.borrowed_books and book.return_book():
+            self.borrowed_books.remove(book)
+            return True
+        return False
     
