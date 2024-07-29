0034b37e8b3d9f3edb766d3ff8f52db35970277f
samwanyua <samexample8@gmail.com>
2024-07-29 21:18:55 +0300

    Hashmaps in details

--- Data Structures/HashMaps.py
+++ Data Structures/HashMaps.py
@@ -1,5 +1,13 @@
 # Hashmaps/ Dictionaries - store key-value pairs
 """
+- Each element in a hashmap consists of a key and a value.
+- Keys must be unique and immutable (like strings, numbers, or tuples),
+while values can be of any data type.
 
+- A hash function is applied to the key to generate a hashcode which is a numerical value
+- Hash code determines the index or "bucket" where key-value pair will be stored in the hashmap
+- Collision handling (keys generating same hashcode) hashmaps employ below techniques:
+    * chaining - storing multiple key-value pairs in the same bucket as a linked list
+    * open addressing - finding an alternative buckets to handle collisions
 
 """
