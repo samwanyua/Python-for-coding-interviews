168aea971690c53d18c8ad69a2866faf8cf648d2
samwanyua <samexample8@gmail.com>
2024-08-07 12:39:10 +0300

    Common string questions

---
+++ Data Structures/strings/strings.py
@@ -0,0 +1,28 @@
+# reverse string
+name = "Joseph"
+print(name[::-1])
+
+
+# check if palindrome
+def is_palindrome(s):
+    return s == s[::-1]
+print(is_palindrome("amas"))
+
+# slicing strings
+s = "abcdefg"
+
+# extract substring from index 1 to 4 (excluding 4)
+sub1 = s[1:4] # "bcd"
+# extract from beginning to index 3(excluding 3)
+sub2 = s[:3] # "abc"
+# extract from index 2 to the end
+sub3 = s[2:] # "cdefg"
+# extract entire string
+sub4 = s[:] # "abcdefg"
+# extract a string with a step of 2
+sub5 = s[::2] # "aceg"
+# extract the last 3 with negative index
+sub6 = s[-3:] # "efg"
+# reverse string
+sub7 = s[::-1]
+
