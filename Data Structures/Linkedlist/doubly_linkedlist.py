c248df6db6285a2d844b4e5afb412f39536763cc
samwanyua <samexample8@gmail.com>
2024-08-08 23:19:46 +0300

    Insert at the tail doubly linked lists

--- Data Structures/Linkedlist/doubly_linkedlist.py
+++ Data Structures/Linkedlist/doubly_linkedlist.py
@@ -6,32 +6,41 @@ class DoublyNode:
     
     def __str__(self) -> str:
         return str(self.val)
-    
+
+    # Display the linked list
+    def display(self):
+        curr = self
+        elements = []
+        while curr:
+            elements.append(str(curr.val))
+            curr = curr.next
+        print(' <-> '.join(elements))
+
+    # Insert at the beginning - O(1)
+    @staticmethod
+    def insert_first(head, tail, val):
+        new_node = DoublyNode(val, next=head)
+        head.prev = new_node
+        return new_node, tail
+
+    # Insert at the end - O(1)
+    @staticmethod
+    def insert_last(head, tail, val):
+        new_node = DoublyNode(val, prev=tail)
+        tail.next = new_node
+        return head, new_node
+
+# Create the head and tail of the doubly linked list
 head = tail = DoublyNode(1)
 print(head, tail)
 
-# display
-def display(head):
-    curr = head
-    elements = []
-    while curr:
-        elements.append(str(curr.val))
-        curr = curr.next
-    print(' <-> '.join(elements))
-display(head)
+# Display the list
+head.display()
 
-# insert at the beginning - O(1)
-def insert_first(head, tail, val):
-    new_node = DoublyNode(val, next=head)
-    head.prev = new_node
-    return new_node, tail
-head,tail = insert_first(head, tail, 3)
-display(head)
+# Insert at the beginning
+head, tail = DoublyNode.insert_first(head, tail, 3)
+head.display()
 
-# insert at the end - O(1)
-def insert_last(head, tail, val):
-    new_node = DoublyNode(val, prev=tail)
-    tail.next = new_node
-    return head, new_node
-head,tail = insert_last(head, tail, 43)
-display(head)
+# Insert at the end
+head, tail = DoublyNode.insert_last(head, tail, 43)
+head.display()
