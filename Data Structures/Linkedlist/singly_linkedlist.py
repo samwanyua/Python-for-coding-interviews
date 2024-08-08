61ab1daef440a7e1dc58528081c4e909cf4e358f
samwanyua <samexample8@gmail.com>
2024-08-08 22:45:04 +0300

    singly linked list

--- Data Structures/Linkedlist/singly_linkedlist.py
+++ Data Structures/Linkedlist/singly_linkedlist.py
@@ -5,7 +5,16 @@ class SinglyNode:
         self.next = next
 
     def __str__(self) -> str:
-        return str(self.val)
+        return str(self.value)
+    # display linked list
+    def display(self):
+        current = self
+        elements = []
+        while current:
+            elements.append(str(current.value))
+            current = current.next
+        print(' -> '.join(elements))
+
     
 Head = SinglyNode(1)
 A = SinglyNode(3)
@@ -17,3 +26,15 @@ Head.next = A
 A.next = B
 B.next = C
 C.next = D
+
+print(Head)
+
+# Traverse the list - O(n)
+current = Head
+while current:
+    print(current)
+    current = current.next
+
+# display
+Head.display()
+
