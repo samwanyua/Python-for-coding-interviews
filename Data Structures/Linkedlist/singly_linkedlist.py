bee53625eb43a61bec1d0ff062d9dae64fe8dcd9
samwanyua <samexample8@gmail.com>
2024-08-08 22:34:59 +0300

    singly linked list

--- Data Structures/Linkedlist/singly_linkedlist.py
+++ Data Structures/Linkedlist/singly_linkedlist.py
@@ -1,1 +1,19 @@
 # singly linked list
+class SinglyNode:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def __str__(self) -> str:
+        return str(self.val)
+    
+Head = SinglyNode(1)
+A = SinglyNode(3)
+B = SinglyNode(4)
+C = SinglyNode(7)
+D = SinglyNode(10)
+
+Head.next = A
+A.next = B
+B.next = C
+C.next = D
