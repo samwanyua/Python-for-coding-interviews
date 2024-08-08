16ea2e08768759f406371b3192f9f38e9ce70454
samwanyua <samexample8@gmail.com>
2024-08-08 23:07:33 +0300

    Implement doubly linked lists

--- Data Structures/Linkedlist/doubly_linkedlist.py
+++ Data Structures/Linkedlist/doubly_linkedlist.py
@@ -1,8 +1,11 @@
 class DoublyNode:
-    def __init__(self, val, next, prev) -> None:
+    def __init__(self, val, next=None, prev=None) -> None:
         self.val = val
         self.next = next
         self.prev = prev
     
     def __str__(self) -> str:
         return str(self.val)
+    
+head = tail = DoublyNode(1)
+print(head, tail)
